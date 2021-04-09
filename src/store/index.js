ccc9946f9359c90f1f42c7b58cfe850699ab46ea
creator-krixus <wilsonrueda2@gmail.com>
2021-04-09 00:33:11 -0500

    add component card

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-      beru: "../assets/images/r5-d4.png",
-
+    people: []
   },
   mutations: {
   },
   actions: {
+    async data(){
+      try{
+        await axios.get('https://swapi.dev/api/people')
+          .then(res => {
+            const data = res.data
+            this.people = data
+            console.log(data.results)
+          })
+      }catch(error){
+        console.log("Error")
+      }
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/images/Beru.png" width="400" height="300">
-   
+    <Card />
+    {{ people }}
+    <button @click="data">LLamar</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import Card from '../components/Card'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
-   
+   Card
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+    ...mapState(['people']),
+    ...mapActions(['data'])
   }
 }
 </script>
