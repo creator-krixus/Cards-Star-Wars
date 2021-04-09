a0c3fdf5018734e593104fafcb7920df41f1ebdb
creator-krixus <wilsonrueda2@gmail.com>
2021-04-09 04:18:03 -0500

    incorporation of dynamic data in the card brought from vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,18 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    people: []
+    people: [],
+    planets: []
   },
   mutations: {
+    setPeople(state, payload){
+      state.people = payload
+    },
+    setPlanets(state, payload){
+      state.planets = payload
+    }
   },
   actions: {
-    async data(){
+    async data({commit}){
       try{
         await axios.get('https://swapi.dev/api/people')
           .then(res => {
             const data = res.data
             this.people = data
-            console.log(data.results)
+            commit('setPeople', data.results)
+ /*            console.log(data)
+            console.log(data.results) */
+          })
+      }catch(error){
+        console.log("Error")
+      }
+    },
+    async planet({commit}){
+      try{
+        await axios.get('https://swapi.dev/api/planets')
+          .then(res => {
+            const data = res.data
+            this.planets = data.results
+            commit('setPlanets', data.results)
+            console.log(this.planets)
           })
       }catch(error){
         console.log("Error")
